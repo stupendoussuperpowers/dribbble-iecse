c412bac3f60a7789c2eb515f331e6ff007bdc8b0
sanchit sahay <sanchitsahay.tech@gmail.com>
2020-05-05 08:54:41 +0530

    env example

--- api/routes/uploadaws.js
+++ api/routes/uploadaws.js
@@ -5,9 +5,11 @@ const multer = require('multer')
 const multers3 = require('multer-s3')
 const aws = require('aws-sdk')
 
+console.log(process.env.AWS_SECRET)
+
 aws.config.update({
-    accessKeyId: process.env.AWS_ACCESS,
-    secretAccessKey: process.env.AWS_KEY,
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
     region: process.env.REGION
 })
 
